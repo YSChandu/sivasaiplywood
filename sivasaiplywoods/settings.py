e31f2afa2ccc0be87c144ba187ba10c9fca078e7
YSChandu <yarrapothusaichandu@gmail.com>
2025-02-23 19:36:08 +0530

    2

--- sivasaiplywoods/settings.py
+++ sivasaiplywoods/settings.py
@@ -128,12 +128,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = "/static/"
+
+# ✅ Add this line
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # This is required for collectstatic to work in production
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# ✅ Ensure Whitenoise is used for serving static files
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
